aaea9526c5efa18174e07fa1114607d86083c761
Stanhoucke <stanhoucke@gmail.com>
2021-03-06 18:04:53 +0000

    can fetch players from API

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Request from '../helpers/Request';
 
 const Leaderboard = () => {
+
+    const [players, setPlayers] = useState([]);
+
+    
+    const getPlayers = () => {
+        fetch("/api/players/")
+        .then(res => res.json())
+        .then(data => setPlayers(data))
+    }
+    // const getPlayers = function(){
+    //     const request = new Request();
+    //     const playersRequest = request.get("/api/players")
+        
+    //     Promise.all([playersRequest])
+    //     .then((data) => {
+    //         setPlayers(data[0])
+    //     })
+    // }
+
+
+    useEffect(() => {
+       getPlayers() 
+    }, [])
+
+    const playerNodes = players.map((player, index) => {
+        return(
+            <p>{player.name}</p>
+        )
+    })
+
+
     return(
+        <>
         <h5>Leaderboard</h5>
+        {playerNodes}
+        </>
     )
 }
 
