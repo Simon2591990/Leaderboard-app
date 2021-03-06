bf0a34cb89de4cc6e950c05d3e61b071f5814a8e
Stanhoucke <stanhoucke@gmail.com>
2021-03-06 16:49:01 +0000

    initial component planning

--- backend/src/main/java/com/codeclan/example/leaderboard_app/components/DataLoader.java
+++ backend/src/main/java/com/codeclan/example/leaderboard_app/components/DataLoader.java
@@ -38,26 +38,104 @@ public class DataLoader implements ApplicationRunner {
 
         Match match1 = new Match (1,season1);
         matchRepository.save(match1);
+        Match match2 = new Match (2,season1);
+        matchRepository.save(match2);
+        Match match3 = new Match (3,season1);
+        matchRepository.save(match3);
 
         Team team1 = new Team("Team 1", match1);
+        Team team2 = new Team("Team 2", match1);
+
+        Team team3 = new Team("Team 3", match2);
+        Team team4 = new Team("Team 4", match2);
+
+        Team team5 = new Team("Team 5", match3);
+        Team team6 = new Team("Team 6", match3);
 
         Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+        Player player4 = new Player("Player 4");
+        Player player5 = new Player("Player 5");
+        Player player6 = new Player("Player 6");
+        Player player7 = new Player("Player 7");
+        Player player8 = new Player("Player 8");
+        Player player9 = new Player("Player 9");
+        Player player10 = new Player("Player 10");
         playerRepository.save(player1);
+        playerRepository.save(player2);
+        playerRepository.save(player3);
+        playerRepository.save(player4);
+        playerRepository.save(player5);
+        playerRepository.save(player6);
+        playerRepository.save(player7);
+        playerRepository.save(player8);
+        playerRepository.save(player9);
+        playerRepository.save(player10);
+
         team1.addPlayer(player1);
+        team1.addPlayer(player2);
+        team1.addPlayer(player3);
+        team1.addPlayer(player4);
+        team1.addPlayer(player5);
         teamRepository.save(team1);
+
+        team2.addPlayer(player6);
+        team2.addPlayer(player7);
+        team2.addPlayer(player8);
+        team2.addPlayer(player9);
+        team2.addPlayer(player10);
+        teamRepository.save(team2);
+
+        team3.addPlayer(player1);
+        team3.addPlayer(player2);
+        team3.addPlayer(player3);
+        team3.addPlayer(player4);
+        team3.addPlayer(player5);
+        teamRepository.save(team3);
+
+        team4.addPlayer(player6);
+        team4.addPlayer(player7);
+        team4.addPlayer(player8);
+        team4.addPlayer(player9);
+        team4.addPlayer(player10);
+        teamRepository.save(team4);
+
+        team5.addPlayer(player1);
+        team5.addPlayer(player2);
+        team5.addPlayer(player3);
+        team5.addPlayer(player4);
+        team5.addPlayer(player5);
+        teamRepository.save(team5);
+
+        team6.addPlayer(player6);
+        team6.addPlayer(player7);
+        team6.addPlayer(player8);
+        team6.addPlayer(player9);
+        team6.addPlayer(player10);
+        teamRepository.save(team6);
+
         match1.addTeam(team1);
+        match1.addTeam(team2);
         matchRepository.save(match1);
+
+        match2.addTeam(team3);
+        match2.addTeam(team4);
+        matchRepository.save(match2);
+
+        match3.addTeam(team5);
+        match3.addTeam(team6);
+        matchRepository.save(match3);
+
         season1.addMatch(match1);
+        season1.addMatch(match2);
+        season1.addMatch(match3);
         seasonRepository.save(season1);
 
-        Player player2 = new Player("Player 2");
-        playerRepository.save(player2);
-        Player player3 = new Player("Player 3");
-        playerRepository.save(player3);
-        ArrayList<Player> players = new ArrayList<Player>();
-        players.add(player2);
-        players.add(player3);
-        team1.addPlayer(players);
-        teamRepository.save(team1);
+//        ArrayList<Player> players = new ArrayList<Player>();
+//        players.add(player2);
+//        players.add(player3);
+//        team1.addPlayer(players);
+//        teamRepository.save(team1);
     }
 }
