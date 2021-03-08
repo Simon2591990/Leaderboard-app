acdd2f20b4c5d1a241cbdb37f6eac30e0821ac15
fukiran <macdorphoto@yahoo.com>
2021-03-08 20:58:36 +0000

    add fetch allplayers

--- frontend/src/containers/CreateSeason.js
+++ frontend/src/containers/CreateSeason.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import SeasonResults from './SeasonResults';
+import React, { useEffect, useState } from 'react';
+import Request from '../helpers/Request';
 
 const CreateSeason = ({seasons}) => {
 
-const playerNodes = seasons[0].players.map((player, index) => {
+
+const [allPlayers, setAllPlayers] = useState([])
+
+const getAllPlayers = () => {
+    const reqest = new Request();
+    reqest.get("/api/players")
+    .then(data => setAllPlayers(data))
+}
+
+useEffect(() => {
+    getAllPlayers() 
+ }, [])
+
+const playerNodes = allPlayers.map((player, index) => {
     return( 
           <option key={index} value={index}>{player.name}</option>
-        )
-       
-    
+        )  
 })
 
     return(
         <>
         <h1>CreateSeason</h1>
         
-        <select name="select_players" defaultValue="selected">
-            <option  disabled value="selected">Select 10 Players</option>
+        <select name="select_players" defaultValue="">
+            <option  disabled value="">Select 10 Players</option>
             {playerNodes}
         </select>
-         
-         <button>Add player to the list</button>
+        <button>Add player to the list</button>
         </>
     )
 }
