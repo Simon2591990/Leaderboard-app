bcbdbcc15c2157c82b6b2e7872fb91cdaafa213d
fukiran <macdorphoto@yahoo.com>
2021-03-09 00:02:20 +0000

    add players to the state

--- frontend/src/containers/CreateSeason.js
+++ frontend/src/containers/CreateSeason.js
@@ -6,6 +6,8 @@ const CreateSeason = ({seasons}) => {
 
 const [allPlayers, setAllPlayers] = useState([])
 
+const [selectedPlayers, setSelectedPlayers] = useState([])
+
 const getAllPlayers = () => {
     const reqest = new Request();
     reqest.get("/api/players")
@@ -16,21 +18,41 @@ useEffect(() => {
     getAllPlayers() 
  }, [])
 
-const playerNodes = allPlayers.map((player, index) => {
+const allPlayerNodes = allPlayers.map((player, index) => {
+    
+
     return( 
-          <option key={index} value={index}>{player.name}</option>
+        
+          <option key={player.id} value={player.id}>{player.name}</option>
         )  
 })
+
+const selectedPlayersNodes = selectedPlayers.map((player, index) => {
+    return(
+        <li>{player.name}</li>
+    )
+})
+
+const handleAddPlayerToTheList = (event) => {
+    event.preventDefault()
+    setSelectedPlayers([...selectedPlayers,allPlayers[event.target.value]])
+}
 
     return(
         <>
         <h1>CreateSeason</h1>
+
+        <form onSubmit={handleAddPlayerToTheList}>
+            <label>  Select 10 players from this list:  
+            <select name="select_players"  value="Select 10 players from this list:  " onChange={handleAddPlayerToTheList}>
+                {allPlayerNodes}
+            </select>
+            </label>
+        </form>
+        <ul>
+            {selectedPlayersNodes}
+        </ul>
         
-        <select name="select_players" defaultValue="">
-            <option  disabled value="">Select 10 Players</option>
-            {playerNodes}
-        </select>
-        <button>Add player to the list</button>
         </>
     )
 }
