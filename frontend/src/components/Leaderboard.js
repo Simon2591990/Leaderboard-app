d849ea3d1043e33e43ed6f7e5b2be222b5bc1f15
Simon Charters <simon259@hotmail.co.uk>
2021-03-10 21:06:57 +0000

    fixed leaderboard and populated dataloader

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './Leaderboard.css';
 import Request from '../helpers/Request';
 
-const Leaderboard = ({players, currentSeasonName}) => {
+const Leaderboard = ({ allPlayers, players, currentSeasonName}) => {
+
+    const [leaderboardSelected, setLeaderboardSelected] = useState("current")
+
+
 
     const assignMedal = (index) => {
         if (index === 0) {
@@ -15,7 +19,10 @@ const Leaderboard = ({players, currentSeasonName}) => {
             return <></>
         }
     }
-
+    const changeLeaderboard = (leaderboard) => {
+        setLeaderboardSelected(leaderboard)
+    }
+        
     const playerNodes = players.map((player, index) => {
         return(
                 <tr key={player.id}
@@ -30,28 +37,71 @@ const Leaderboard = ({players, currentSeasonName}) => {
                 </tr>
         )
     })
+    const allPlayerNodes = allPlayers.map((player, index) => {
+        return(
+                <tr key={player.id}
+                className="leaderboard-table-row">
+                    <td>{index + 1}  {assignMedal(index)}</td>
+                    <td>{player.name}</td>
+                    <td>{player.totalGamesPlayed}</td>
+                    <td>{player.totalGamesWon}</td>
+                    <td>{player.totalGamesDrawn}</td>
+                    <td>{player.totalGamesLost}</td>
+                    <td>{player.totalPoints}</td>
+                </tr>
+        )
+        })
+    
 
-    return(
-        <>
-        <h2>Leaderboard - {currentSeasonName}</h2>
-        <table className="leaderboard-table">
-                    <thead>
-                        <tr className="leaderboard-table-header">
-                            <th>Postition</th>
-                            <th>Player</th>
-                            <th>Played</th>
-                            <th>Won</th>
-                            <th>Drawn</th>
-                            <th>Lost</th>
-                            <th>Points</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {playerNodes}
-                    </tbody>
-                </table>
-        </>
-    )
+    if (leaderboardSelected === "current"){
+        return(
+            <>
+            <h2>Leaderboard - {currentSeasonName}</h2>
+            
+            <button onClick={() => changeLeaderboard("all_time")}>Current Season</button>
+            <table className="leaderboard-table">
+                        <thead>
+                            <tr className="leaderboard-table-header">
+                                <th>Postition</th>
+                                <th>Player</th>
+                                <th>Played</th>
+                                <th>Won</th>
+                                <th>Drawn</th>
+                                <th>Lost</th>
+                                <th>Points</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {playerNodes}
+                        </tbody>
+                    </table>
+            </>
+        )
+    }
+    if (leaderboardSelected === "all_time"){
+        return(
+            <>
+            <h2>All Time Leaderboard</h2>
+            <button onClick={() => changeLeaderboard("current")}>Current Season</button>
+            <table className="leaderboard-table">
+                        <thead>
+                            <tr className="leaderboard-table-header">
+                                <th>Postition</th>
+                                <th>Player</th>
+                                <th>Played</th>
+                                <th>Won</th>
+                                <th>Drawn</th>
+                                <th>Lost</th>
+                                <th>Points</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {allPlayerNodes}
+                        </tbody>
+                    </table>
+            </>
+        )
+    }
 }
 
 export default Leaderboard;
