c00d2845b652102d3534db40901cb8a36f7432c7
Stanhoucke <stanhoucke@gmail.com>
2021-03-10 18:18:06 +0000

    flexbox to accordion

--- frontend/src/components/MatchDetails.js
+++ frontend/src/components/MatchDetails.js
@@ -11,12 +11,12 @@ import './MatchDetails.css';
 const MatchDetails = ({match}) => {
     const team1PlayerNodes = match.teams[0].players.map((player, index) => {
         return(
-            <li key={player.id} >{player.name}</li>
+            <p key={player.id} >{player.name}</p>
         )
     })
     const team2PlayerNodes = match.teams[1].players.map((player, index) => {
         return(
-            <li key={player.id} >{player.name}</li>
+            <p key={player.id} >{player.name}</p>
         )
     })
 
@@ -30,20 +30,26 @@ const MatchDetails = ({match}) => {
             <AccordionItem>
                 <AccordionItemHeading>
                     <AccordionItemButton>
-                    <div>
-                        <h3>{match.teams[0].result}</h3>
-                        <h2>{match.teams[0].name}</h2>
-                        <h2>{match.teams[0].goals}</h2>
-                        <h1>vs</h1>
-                        <h2>{match.teams[1].goals}</h2>
-                        <h2>{match.teams[1].name}</h2>
-                        <h3>{match.teams[1].result}</h3>
+                    <div className="match-result">
+                        <h3 className="match-result-result">{match.teams[0].result}</h3>
+                        <h2 className="match-result-name">{match.teams[0].name}</h2>
+                        <h2 className="match-result-goals">{match.teams[0].goals}</h2>
+                        <h1 className="match-result-vs">vs</h1>
+                        <h2 className="match-result-goals">{match.teams[1].goals}</h2>
+                        <h2 className="match-result-name">{match.teams[1].name}</h2>
+                        <h3 className="match-result-result">{match.teams[1].result}</h3>
                     </div>
                     </AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
-                    {team1PlayerNodes}
-                    {team2PlayerNodes}
+                    <div className="teams">
+                        <div className="team-players">
+                            {team1PlayerNodes}
+                        </div>
+                        <div className="team-players">
+                            {team2PlayerNodes}
+                        </div>
+                    </div>
                 </AccordionItemPanel>
             </AccordionItem>
         </>
